5a1816207634912f66afa6a83293732e81a970ae
prateeksha144 <166383321+prateeksha144@users.noreply.github.com>
2024-05-14 15:25:48 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://dream-look-ecommerce-4.onrender.com/api/v2";
 
-export const backend_url = "https://dream-look-ecommerce-4.onrender.com";
+export const backend_url = "https://dream-look-ecommerce-4.onrender.com/";
